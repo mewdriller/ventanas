3de8eb3b24953ff00b5c866ea6ed119b28f3015d
Drew Miller <dmiller@base2.io>
2012-08-19 15:55:06 -0400

    updated to use icon for installer as well

--- Ventanas/Properties/AssemblyInfo.cs
+++ Ventanas/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("Ventanas")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Base Two Interactive")]
 [assembly: AssemblyProduct("Ventanas")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
